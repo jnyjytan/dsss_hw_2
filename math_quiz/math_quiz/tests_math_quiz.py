08bceb8783f5a055291172f1bc840968dab6444a
unknown <jieyi.tan@fau.de>
2023-11-13 18:16:22 +0100

    update unit tests in the file test_math_quiz.py

--- math_quiz/math_quiz/tests_math_quiz.py
+++ math_quiz/math_quiz/tests_math_quiz.py
@@ -1,30 +1,42 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import get_random_integer, get_random_math_symbol, plus_minus_multiplication
 
 
 class TestMathGame(unittest.TestCase):
+    
+    def are_elements_string(elements):
+        return all(isinstance(element, str) for element in elements)
 
-    def test_function_A(self):
+    def test_get_random_integer(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = get_random_integer(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
+    def test_get_random_math_symbol(self):
         # TODO there is something to be done
-        pass
+        self.assertIsInstance(get_random_math_symbol(),str)
 
-    def test_function_C(self):
+        #pass
+
+    def test_plus_minus_multiplication(self):
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+                (3, 2, '-', '3 - 2', 1 ),
+                (5, 2, '*', '5 * 2', 10)
+                #''' TODO add more test cases here '''
             ]
 
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+                
+                    pass
+
+                    printed, result = plus_minus_multiplication(num1,num2,operator)
+                    self.assertEqual(result, expected_answer)
+
+        
 
 if __name__ == "__main__":
     unittest.main()
