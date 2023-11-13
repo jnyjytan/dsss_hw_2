4731a1bcdd119be885d0e864c35880973706d7b3
unknown <jieyi.tan@fau.de>
2023-11-13 17:19:09 +0100

    File 'math_quiz.py' was edited with more comments, docstring, bug fix and handling of errors

--- math_quiz/math_quiz/math_quiz.py
+++ math_quiz/math_quiz/math_quiz.py
@@ -1,35 +1,93 @@
 import random
+import math
 
 
-def function_A(min, max):
+def get_random_integer(min, max):
     """
-    Random integer.
+    Return a random integer between given input values (min and max).
+    
+    Parameters
+    ----------
+    min: integer 
+        The smallest random number that can be returned
+        
+    max: integer
+        The largest random number that can be returned
+        
+    Returns
+    -------
+    integer
+        A random value produced by the function random.randint()
+       
     """
-    return random.randint(min, max)
+    try: 
+        return random.randint(min, max)
+    except ValueError: 
+        print("Function 'get_random_integer()': input(s) not an integer")
 
 
-def function_B():
+def get_random_math_symbol():
+    """
+    Return a random mathematical operation symbol.
+    
+    Parameters
+    ----------
+    None
+        
+    Returns
+    -------
+    string
+        A random element from a list of mathematical operation symbols.
+       
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
+def plus_minus_multiplication(n1, n2, o):
+    """
+    Return a integer value after executing a defined mathematical operation on two given 
+    values.
+    The following symbols/operations are accepted:
+    '+': addition
+    '-': subtraction
+    '*': multiplication
+    
+    Parameters
+    ----------
+    n1: integer
+        first value 
+    n2: integer
+        second value
+    o: 
+        mathematical operation symbol
+        
+    Returns
+    -------
+    p: string
+        prints the mathematical equation
+    a: integer
+        An value resulted from the execution of a defined mathematical operation on two 
+        input integers
+    """
+       
     p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
+    if o == '+': a = n1 + n2
+    elif o == '-': a = n1 - n2
     else: a = n1 * n2
     return p, a
 
 def math_quiz():
     s = 0
-    t_q = 3.14159265359
+    t_q = math.ceil(3.14159265359)
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
+    # how many questions in a quiz game  
     for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+        n1 = get_random_integer(1, 10); n2 = get_random_integer(1, 5); o = get_random_math_symbol()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        PROBLEM, ANSWER = plus_minus_multiplication(n1, n2, o)
         print(f"\nQuestion: {PROBLEM}")
         useranswer = input("Your answer: ")
         useranswer = int(useranswer)
